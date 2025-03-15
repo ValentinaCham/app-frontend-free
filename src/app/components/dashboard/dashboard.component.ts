bcd96fc9636e69bcfa9c5d2421a7243d44f3c734
Valentina Chambilla <vchambillap@unsa.edu.oe>
2025-03-15 10:06:30 -0500

    edit: Dashboard, NavBar and Spinner

--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,8 +1,10 @@
 import { Component } from '@angular/core';
+import { NavbarComponent } from '../navbar/navbar.component';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [NavbarComponent],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.css'
 })
